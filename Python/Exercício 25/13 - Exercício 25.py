6aa820f67820ebf22fe5821def2e109c5e1eb5ba
Matta <159219188+Matta3215@users.noreply.github.com>
2024-11-05 17:26:19 -0300

    Delete Python/Exercício 25/13 - Exercício 25.py

--- Python/Exercício 25/13 - Exercício 25.py
+++
@@ -1,7 +0,0 @@
-vetor = []
-for x in range(10):
-    num = int(input("Digite um número: "))
-    vetor.append(num)
-    
-print(f"\nVetor original: {vetor}")
-print(f"Elementos iguais no vetor: {list(set(vetor))}\n")
