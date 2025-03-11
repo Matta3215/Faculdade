7a8a6bc5bcbe8933f98da9632f2b2078f5247519
Matta <159219188+Matta3215@users.noreply.github.com>
2025-03-11 15:22:51 -0300

    Update Conta - 1.java

--- Java/Conta/Conta.java
+++ Java/Conta/Conta.java
@@ -1,15 +1,16 @@
 public class Conta {
-    String nome;
     int numero;
     double limite;
     double saldo;
-    
+    Cliente titular;
     
     void Info_Dados(){
         System.out.println("\n===================================");
         System.out.println("               CONTA");
         System.out.println("===================================");
-        System.out.printf("Nome: %s", nome);
+        System.out.printf("Nome: %s", titular.nome);
+        System.out.printf("\nSobrenome: %s", titular.sobrenome);
+        System.out.printf("\nCPF: %s", titular.cpf);
         System.out.printf("\nNúmero da Conta: %d", numero);
         System.out.printf("\nSeu saldo é:  R$ %.2f", saldo);
         System.out.printf("\nSeu limite é: R$ %.2f", limite);
