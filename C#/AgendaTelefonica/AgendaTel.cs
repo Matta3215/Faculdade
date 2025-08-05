8eb48cf6635037b4f4019ccdc20255d85e04602e
Matta <159219188+Matta3215@users.noreply.github.com>
2025-08-05 16:57:54 -0300

    Add files via upload

---
+++ C#/AgendaTelefonica/AgendaTel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class AgendaTel
+{
+	public AgendaTel()
+	{
+        Dictionary<string, string> agenda = new();
+
+        public void Inserir(string nome, string numero)
+    {
+        agenda.Add(nome, numero);
+    }
+
+    public string BuscarNumero(string numero)
+    {
+        Console.WriteLine($"Número:{agenda[numero]}");
+    }
+
+    public void Remover(string nome)
+    {
+
+    }
+
+    public int tamanho()
+    {
+
+    }
+}
+}
