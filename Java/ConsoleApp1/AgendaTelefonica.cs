73e6c930e49de9227a5585e49d20ce16203ff100
Matta <159219188+Matta3215@users.noreply.github.com>
2025-08-05 08:53:36 -0300

    Add files via upload

---
+++ Java/ConsoleApp1/AgendaTelefonica.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AgendaTelefonica
+    {
+        Dictionary<string, string> agenda = new();
+
+        public void Inserir(string nome, string numero)
+        {
+            agenda.Add(nome, numero);            
+        }
+
+        public string BuscarNumero(string numero)
+        {
+            Console.WriteLine($"Númerp:{agenda[numero]}");
+        }
+
+        public void Remover(string nome)
+        {
+
+        }
+
+        public int tamanho()
+        {
+
+        }
+    }
+}
